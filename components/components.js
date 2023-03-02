58d38f8f0668a72246af9cf9b6b0f0cff1da2b13
mhking20 <www.taimoorking20@gmail.com>
2023-03-02 14:32:10 +0500

    Okey Hai Sab

--- components/components.js
+++ components/components.js
@@ -1,13 +1,15 @@
 const model = require("../model/model");
 const jwt = require("jsonwebtoken");
-require("dotenv").config()
+require("dotenv").config();
 
 const postuser = async (req, res) => {
   try {
     const post = await model.create(req.body);
+    console.log(post);
     const token = jwt.sign(
       { UserID: post._id, username: post.username },
-      process.env.JWT_SECRET , {expiresIn : "1d"}
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
     );
     res.status(201).json({ msg: "User Created", token });
   } catch (error) {
@@ -89,7 +91,8 @@ const loginuser = async (req, res) => {
     if (user) {
       const token = jwt.sign(
         { UserID: user._id, username: user.username },
-        process.env.JWT_SECRET , {expiresIn : "1d"}
+        process.env.JWT_SECRET,
+        { expiresIn: "1d" }
       );
       res.json({ token });
     }
@@ -104,10 +107,27 @@ const auth = async (req, res) => {
     const token = headertoken.split(" ")[1];
     console.log(token);
     const auth = jwt.verify(token, process.env.JWT_SECRET);
-    console.log({auth});
-    res.json({auth})
+    console.log({ auth });
+    res.json({ auth });
   } catch (error) {
-    res.json({msg : "Unauthorized"})
+    res.json({ msg: "Unauthorized" });
+    console.log(error);
+  }
+};
+
+const reguser = async (req, res) => {
+  try {
+    const data = req.query.data;
+    const email = await model.findOne({ email: data.email });
+    const username = await model.findOne({ username: data.username });
+    if(email){
+      res.json({msg : "Email Already Exist"})
+    }
+    if(username){
+      res.json({msg : "Username Already Exist"})
+    }
+    res.json({data})
+  } catch (error) {
     console.log(error);
   }
 };
@@ -120,4 +140,5 @@ module.exports = {
   deleteuser,
   loginuser,
   auth,
+  reguser,
 };
--- routers/routers.js
+++ routers/routers.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
-const { postuser , getuser , getsingleuser , updateuser , deleteuser , loginuser , auth} = require("../components/components");
+const { postuser , getuser , getsingleuser , updateuser , deleteuser , loginuser , auth , reguser} = require("../components/components");
 
 router.route("/").post(postuser).get(getuser);
 router.route("/user").get(getsingleuser).patch(updateuser).delete(deleteuser)
 router.route("/login").get(loginuser)
 router.route("/auth").get(auth)
+router.route("/reg").get(reguser)
 
 module.exports = router;
