0083d934928af0621ced06b2205533dcc6441437
mhking20 <113695912+mhking20@users.noreply.github.com>
2023-03-08 06:10:44 +0000

    i

--- index.js
+++ index.js
@@ -5,10 +5,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const cors = require('cors');
-app.use(cors({
-  origin : "https://mian-first-web.netlify.app",
-  methods : ["GET" , "POST" , "DELETE" , "PATCH"]
-}))
+app.use(cors())
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
